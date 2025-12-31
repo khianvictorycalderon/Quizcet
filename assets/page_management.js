20820e172d0961211cbb1d6b3bb59d67d8cb24fd
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-31 13:16:33 +0800

    Separated pages

--- assets/page_management.js
+++ assets/page_management.js
@@ -1,27 +1,3 @@
-// HTML Contents
-const pages = {
-    home: `
-        <div class="min-h-screen pt-20">
-            <h2>This is Home page</h2>
-        </div>
-    `,
-    subjects: `
-        <div class="min-h-screen pt-20">
-            <h2>This is Subjects page</h2>
-        </div>
-    `,
-    questions: `
-        <div class="min-h-screen pt-20">
-            <h2>This is Questions page</h2>
-        </div>
-    `,
-    tutorial: `
-        <div class="min-h-screen pt-20">
-            <h2>This is Tutorial page</h2>
-        </div>
-    `
-};
-
 // Function to get the "page" from URL query
 function getPageFromURL() {
     const params = new URLSearchParams(window.location.search);
---
+++ assets/pages.js
@@ -0,0 +1,22 @@
+const pages = {
+    home: `
+        <div class="min-h-screen pt-20">
+            <h2>This is Home page</h2>
+        </div>
+    `,
+    subjects: `
+        <div class="min-h-screen pt-20">
+            <h2>This is Subjects page</h2>
+        </div>
+    `,
+    questions: `
+        <div class="min-h-screen pt-20">
+            <h2>This is Questions page</h2>
+        </div>
+    `,
+    tutorial: `
+        <div class="min-h-screen pt-20">
+            <h2>This is Tutorial page</h2>
+        </div>
+    `
+};
