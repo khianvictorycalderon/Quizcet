d2b8309040149c899e91993c0c61de78d35f4bf9
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-31 13:22:58 +0800

    Added settings page + working on home page

--- assets/pages.js
+++ assets/pages.js
@@ -2,9 +2,14 @@ const pages = {
     home: `
         <div class="
             px-4 md:px-8 lg:px-16 pb-4
-            min-h-screen pt-20
+            min-h-screen
+            flex flex-col items-center justify-center
             ">
-            <h2>This is Home page</h2>
+            
+            <div>
+                <h2 class="font-extrabold text-xl md:text-2xl lg:text-4xl">GET STARTED!</h2>
+            </div>
+
         </div>
     `,
     subjects: `
@@ -29,6 +34,14 @@ const pages = {
             min-h-screen pt-20
             ">
             <h2>This is Tutorial page</h2>
+        </div>
+    `,
+    settings: `
+        <div class="
+            px-4 md:px-8 lg:px-16 pb-4
+            min-h-screen pt-20
+            ">
+            <h2>This is Settings page</h2>
         </div>
     `
 };
