fb889875ec210f0a8a29f2c81bff587036b447cd
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-31 13:18:11 +0800

    Added x padding

--- assets/pages.js
+++ assets/pages.js
@@ -1,21 +1,33 @@
 const pages = {
     home: `
-        <div class="min-h-screen pt-20">
+        <div class="
+            px-4 md:px-8 lg:px-16 pb-4
+            min-h-screen pt-20
+            ">
             <h2>This is Home page</h2>
         </div>
     `,
     subjects: `
-        <div class="min-h-screen pt-20">
+        <div class="
+            px-4 md:px-8 lg:px-16 pb-4
+            min-h-screen pt-20
+            ">
             <h2>This is Subjects page</h2>
         </div>
     `,
     questions: `
-        <div class="min-h-screen pt-20">
+        <div class="
+            px-4 md:px-8 lg:px-16 pb-4
+            min-h-screen pt-20
+            ">
             <h2>This is Questions page</h2>
         </div>
     `,
     tutorial: `
-        <div class="min-h-screen pt-20">
+        <div class="
+            px-4 md:px-8 lg:px-16 pb-4
+            min-h-screen pt-20
+            ">
             <h2>This is Tutorial page</h2>
         </div>
     `
